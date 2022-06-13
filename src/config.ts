1cbc7532ec7d5a794abbbc4cd2f3c920ec51899e
Rhuan Chaves <rhuan.chaves@vollsolutions.com.br>
2022-06-12 22:32:02 -0300

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,17 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
+  backgroundColor: '#313131',
   scale: {
     width: 800,
     height: 600,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+        debug: true
+    }
+},
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,50 @@
 import Phaser from 'phaser';
 
 export default class Demo extends Phaser.Scene {
+  cursors: any;
+  player: any;
+  ball: any;
   constructor() {
     super('GameScene');
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('balls', 'assets/balls.png', { frameWidth: 17, frameHeight: 17 });
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    this.player = this.add.circle(200, 200, 20, 0x6666ff);
+    this.ball = this.physics.add.image(300, 300, 'balls',1);
+    
+    this.physics.add.existing(this.player, false);
+    this.player.body.setCircle(20);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, this.ball);
+    this.player.body.setCollideWorldBounds(true);
+    this.player.setStrokeStyle(2, 0xefc53f);
+    this.ball.body.setCircle(9)
+    this.ball.body.setCollideWorldBounds(true);
+    this.ball.body.setFrictionX(500);
+    this.ball.body.setBounce(0.6)
+  }
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+  update(time: number, delta: number): void {
+    this.player.body.setVelocity(0);
+    if(this.cursors.space.isDown){
+      this.ball.body.setVelocityX(500);
+    }
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-100);
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(100);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-100);
+    }
+    else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(100);
+    }
   }
 }
